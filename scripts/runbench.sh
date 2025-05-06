07e6ab4b66495884c624342926f2b992a9e8fa9b
Pierre DOnat-Bouillud <pierre.donat.bouillud@gmail.com>
2025-05-06 13:58:41 +0000

    Fix merge mess

--- scripts/runbench.sh
+++ scripts/runbench.sh
@@ -8,11 +8,7 @@ done
 echo "Running the Java implementation"
 JAVA_SERVER_PATH=${JAVA_SERVER_PATH:-"/Users/jakobeha/Documents/grad/research"}
 for i in {1..10}; do
-<<<<<<< HEAD
     (cd "$JAVA_SERVER_PATH/r-compile-server/server" && mvn test -q -Dtest=BCCompilerBenchmarkTest#testBenchmark) 2>/dev/null >> javatimings.csv
-=======
-    (cd "/home/jakob/r-compile-server/server" && mvn test -q -Dtest=BCCompilerBenchmarkTest#testBenchmark) 2>/dev/null >> javatimings.csv
->>>>>>> 66ed4132506b2f27f5f7b5dc998e76190cfd10e3
 done
 
 echo "Running the Rust implementation"
